dc5bc1923228d2ba3f11d8d8c01c66fac1f7cbed
selinarslanbulut <arslanbulutselin@outlook.com>
2022-03-31 16:18:47 +0300

    Homework-1 Complated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/notes/notes.component.ts
@@ -0,0 +1,73 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './notes.Component.html',
+  styleUrls: ['./notes.component.css']
+})
+export class NotesComponent implements OnInit {
+
+  baslik = '';
+  aciklama = '';
+  selected = '';
+  notes: any[] = [];
+  currentIndex = 0;
+  status: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+    this.notes = JSON.parse(localStorage.getItem('notes')||'{}')
+    console.log(this.notes)
+  }
+
+     insertData() {
+    const note = {
+      baslik: this.baslik,
+      aciklama: this.aciklama
+    }
+    const noteItem = JSON.stringify(note)
+    this.addData(noteItem)
+    this.getData();
+  }
+
+  addData(item: string) {
+    const local = localStorage.getItem("notes")
+    if (local) {
+      const noteArray = JSON.parse(local)
+      noteArray.push(JSON.parse(item));
+      localStorage.setItem('notes', JSON.stringify(noteArray))
+    } else {
+      localStorage.setItem('notes', '[' + item + ']')
+    }
+  }
+  selectData(index: number){
+    this.status = true;
+    this.currentIndex = index;
+    this.baslik = this.notes[index].baslik
+    this.aciklama = this.notes[index].aciklama
+    
+  }
+  updateData(index: number) {
+     this.notes[index].baslik = this.baslik
+     this.notes[index].text = this.aciklama;
+    const noteItem = JSON.stringify(this.notes)
+    localStorage.removeItem('notes')
+    localStorage.setItem('notes', noteItem)
+    this.status = false;
+  }
+
+  deleteData(index: number) {
+    this.notes.splice(index, 1)
+    localStorage.setItem('notes', JSON.stringify(this.notes))
+  }
+  getData() {
+    const noteItems = localStorage.getItem('notes')
+    if (noteItems) {
+      this.notes = JSON.parse(noteItems)
+    }
+  }
+}
+
+
